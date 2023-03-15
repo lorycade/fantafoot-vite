29765b1a2651c9a315fcfc81efc50cd51106730b
lorycade <lorenzo.cadente@accenture.com>
2023-03-15 11:40:55 +0100

    remove things from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,9 @@
-import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-lg-8">
-            <h2>Classifica top 5</h2>
-            <div className="leaderboard">
-              <div className="line head">
-                <div className="cell">Posizione</div>
-                <div className="cell">Giocatore</div>
-                <div className="cell">Punti</div>
-              </div>
-              <div className="line body">
-                <div className="cell">1</div>
-                <div className="cell">Antonio Ricci</div>
-                <div className="cell">50</div>
-              </div>
-              <div className="line body">
-                <div className="cell">2</div>
-                <div className="cell">Giuseppe Bianchi</div>
-                <div className="cell">30</div>
-              </div>
-              <div className="line body">
-                <div className="cell">3</div>
-                <div className="cell">Luca Verde</div>
-                <div className="cell">10</div>
-              </div>
-              <div className="line body">
-                <div className="cell">4</div>
-                <div className="cell">Antonio Ricci</div>
-                <div className="cell">50</div>
-              </div>
-              <div className="line body">
-                <div className="cell">5</div>
-                <div className="cell">Giuseppe Bianchi</div>
-                <div className="cell">30</div>
-              </div>
-            </div>
-            <Link to="/classifica" className="d-inline-block mt-4">
-              Vedi tutta la classifica
-            </Link>
-          </div>
-          <div className="col-lg-4">
-            <p>vedremo cosa inserire</p>
-          </div>
-        </div>
+      <div className="container my-5 text-center">
+        <h1>Pagina in allestimento</h1>
       </div>
     </>
   );
