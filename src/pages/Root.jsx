f03811d3ea43342490ece7d824e8839d62d147f5
lorycade <lorenzo.cadente@accenture.com>
2023-03-26 19:20:39 +0200

    temporay unblock

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router-dom";
-// import { useContext } from "react";
-// import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "../components/header/header";
-// import InsertTeamBanner from "../components/insert-team-banner/insert-team-banner";
+import InsertTeamBanner from "../components/insert-team-banner/insert-team-banner";
 
 function Root() {
-  // const {user} = useContext(UserContext)
+  const {user} = useContext(UserContext)
 
   return (
     <>
       <Header />
-      {/* {user && <InsertTeamBanner />} */}
+      {user && <InsertTeamBanner />}
       <Outlet />
     </>
   );
