f023a124a2ad5ccaf1103cb3404e5861e7862de6
lorycade <lorenzo.cadente@accenture.com>
2023-07-21 20:29:03 +0200

    remove banner

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
         { path: '/listone', element: <Listone />},
         { path: '/classifica', element: <Classifica />},
         { path: '/profilo', element: <Profilo />},
-        { path: '/inserisci-formazione', element: <AddLineup />},
+        // { path: '/inserisci-formazione', element: <AddLineup />},
         // { path: '/crea-squadra', element: <CreateTeam />},
         { path: '/login', element: <Login />},
         { path: '/api/auth/email-confirmation', element: <Login />},
